badc956db8a28bc2ea23a682099b695ca79c10fd
Remco Tolsma <info@remcotolsma.nl>
2015-01-27 09:42:41 +0100

    Updated version and copyright year to 2015.

--- src/Extension.php
+++ src/Extension.php
@@ -3,10 +3,10 @@
 /**
  * Title: WordPress pay Event Espresso extension
  * Description:
- * Copyright: Copyright (c) 2005 - 2014
+ * Copyright: Copyright (c) 2005 - 2015
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.1
  */
 class Pronamic_WP_Pay_Extensions_EventEspresso_Extension {
 	/**
