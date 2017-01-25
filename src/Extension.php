92d392ca26b4af736f12750ff11863966bdf74b1
Remco Tolsma <info@remcotolsma.nl>
2017-01-25 13:55:35 +0100

    Getting ready for version 1.1.6.

--- src/Extension.php
+++ src/Extension.php
@@ -3,11 +3,11 @@
 /**
  * Title: WordPress pay Event Espresso extension
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.1.3
+ * @version 1.1.6
  * @since 1.0.2
  */
 class Pronamic_WP_Pay_Extensions_EventEspresso_Extension {
