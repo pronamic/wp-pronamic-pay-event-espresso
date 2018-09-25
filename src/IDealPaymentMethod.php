57c92afaf6637ba70a5a8c789e15b3a48142d642
Reüel van der Steege <reuel@pronamic.nl>
2018-09-25 11:09:41 +0200

    Use updated iDEAL gateway class name.

--- src/IDealPaymentMethod.php
+++ src/IDealPaymentMethod.php
@@ -39,7 +39,7 @@ class IDealPaymentMethod extends PaymentMethod {
 	 * @param EE_Payment_Method $pm_instance Event Espresso payment method instance.
 	 */
 	public function __construct( $pm_instance = null ) {
-		$this->_gateway            = new IDealGateway46();
+		$this->_gateway            = new IDealGateway();
 		$this->_pretty_name        = __( 'iDEAL', 'pronamic_ideal' );
 		$this->_default_button_url = plugins_url( 'images/ideal/icon-64x48.png', Plugin::$file );
 
