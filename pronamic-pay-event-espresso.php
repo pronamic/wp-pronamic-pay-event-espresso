f05ac03d6464ae1e5455224d2554ec915f2ac42d
Reüel van der Steege <reuel@pronamic.nl>
2022-09-27 11:19:58 +0200

    Version bump.

--- pronamic-pay-event-espresso.php
+++ pronamic-pay-event-espresso.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-event-espresso/
  * Description: Extend the Pronamic Pay plugin with Event Espresso support to receive payments through a variety of payment providers.
  *
- * Version: 4.1.0
+ * Version: 4.1.1
  * Requires at least: 4.7
  *
  * Author: Pronamic
